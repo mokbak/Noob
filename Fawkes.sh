35ddfede37f5cda515e7cbbb1fd6d9b5b1ec512a
Mokhtar ELBAKKALI <49704852+mokbak@users.noreply.github.com>
2021-02-15 02:18:37 +0100

    Create Fawkes.sh

---
+++ Fawkes.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo -e "\n"
+echo "#######################################"
+echo "###### Fawkes ######"
+echo -e "#######################################" "\n\n"
+
+echo -e "###### Download fawkes ######" "\n"
+
+curl -O https://raw.githubusercontent.com/mokbak/main/fawkes.zip
+
+echo -e "###### unzip fawkes ######" "\n"
+
+unzip fawkes.zip
+
+echo -e "###### install requirements ######" "\n"
+
+pip install -r requirements.txt
+
+echo -e "###### Run Fawkes ######" "\n"
+
+python fawkes.py
