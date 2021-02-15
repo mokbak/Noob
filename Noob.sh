f6643bc0790355f1fb82b0bf7342906b0b945565
Mokhtar ELBAKKALI <49704852+mokbak@users.noreply.github.com>
2021-02-15 04:29:29 +0100

    Update Noob.sh

--- Noob.sh
+++ Noob.sh
@@ -5,38 +5,52 @@ echo "###### Noob : 'PhpVuln, Fawkes, SQLMAP, ATLAS, Osmedeus, Zeus' ######"
 echo -e "#####################################################################" "\n\n"
 
 echo -e "###### Download ######" "\n"
-curl -O https://raw.githubusercontent.com/mokbak/Noob/main/fawkes.zip
+curl -O https://raw.githubusercontent.com/mokbak/Noob/main/PhpVuln.zip
 echo -e "\n"
-curl -O https://raw.githubusercontent.com/mokbak/Noob/main/fawkes.zip
+curl -O https://raw.githubusercontent.com/mokbak/Noob/main/FAWKES.zip
 echo -e "\n"
-curl -O https://raw.githubusercontent.com/mokbak/Noob/main/fawkes.zip
+curl -O https://raw.githubusercontent.com/mokbak/Noob/main/SQLMAP.zip
 echo -e "\n"
-curl -O https://raw.githubusercontent.com/mokbak/Noob/main/fawkes.zip
+curl -O https://raw.githubusercontent.com/mokbak/Noob/main/ATLAS.zip
+echo -e "\n"
+curl -O https://raw.githubusercontent.com/mokbak/Noob/main/Osmedeus.zip
+echo -e "\n"
+curl -O https://raw.githubusercontent.com/mokbak/Noob/main/Zeus.zip
 
 
 echo -e "\n\n"
 echo -e "###### unzip ######" "\n"
-unzip fawkes.zip
+unzip PhpVuln.zip
 echo -e "\n"
-unzip fawkes.zip
+unzip FAWKES.zip
 echo -e "\n"
-unzip fawkes.zip
+unzip SQLMAP.zip
 echo -e "\n"
-unzip fawkes.zip
+unzip ATLAS.zip
+echo -e "\n"
+unzip Osmedeus.zip
+echo -e "\n"
+unzip Zeus.zip
 
 
 echo -e "\n\n"
 echo -e "###### install requirements ######" "\n"
-pip install -r requirements.txt
-pip install -r requirements.txt
-pip install -r requirements.txt
-pip install -r requirements.txt
+pip install -r /phpvuln/requirements.txt
+pip install -r /fawkes/requirements.txt
+pip install -r /sqlmap/requirements.txt
+pip install -r /osmedeus/requirements.txt
+pip install -r /zeus/requirements.txt
 
 
 echo -e "\n\n"
 echo -e "###### Delete Install *.sh *.zip ######" "\n"
 rm -R sample_data
-rm Sendlight_V5_Colab_Mini_Loader.sh
+rm PhpVuln.zip
+rm FAWKES.zip
+rm SQLMAP.zip
+rm ATLAS.zip
+#unzip Osmedeus.zip
+#unzip Zeus.zip
 
 echo -e "\n"
 echo "################## END ##################"
