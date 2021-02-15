7e5e1614dacc00a8bdbb260db54af7dc6fd2d181
Mokhtar ELBAKKALI <49704852+mokbak@users.noreply.github.com>
2021-02-15 03:59:42 +0100

    Update and rename Fawkes.sh to Noob.sh

---
+++ Noob.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo -e "\n"
+echo "#####################################################################"
+echo "###### Noob : 'PhpVuln, Fawkes, SQLMAP, ATLAS, Osmedeus, Zeus' ######"
+echo -e "#####################################################################" "\n\n"
+
+echo -e "###### Download ######" "\n"
+curl -O https://raw.githubusercontent.com/mokbak/Noob/main/fawkes.zip
+echo -e "\n"
+curl -O https://raw.githubusercontent.com/mokbak/Noob/main/fawkes.zip
+echo -e "\n"
+curl -O https://raw.githubusercontent.com/mokbak/Noob/main/fawkes.zip
+echo -e "\n"
+curl -O https://raw.githubusercontent.com/mokbak/Noob/main/fawkes.zip
+
+
+echo -e "\n\n"
+echo -e "###### unzip ######" "\n"
+unzip fawkes.zip
+echo -e "\n"
+unzip fawkes.zip
+echo -e "\n"
+unzip fawkes.zip
+echo -e "\n"
+unzip fawkes.zip
+
+
+echo -e "\n\n"
+echo -e "###### install requirements ######" "\n"
+pip install -r requirements.txt
+pip install -r requirements.txt
+pip install -r requirements.txt
+pip install -r requirements.txt
+
+
+echo -e "\n\n"
+echo -e "###### Delete Install *.sh *.zip ######" "\n"
+rm -R sample_data
+rm Sendlight_V5_Colab_Mini_Loader.sh
+
+echo -e "\n"
+echo "################## END ##################"
