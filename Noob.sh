0af8a3cefed84bf7b738808a8f1de4db054a2c57
Mokhtar ELBAKKALI <49704852+mokbak@users.noreply.github.com>
2021-02-18 04:31:35 +0100

    Update Noob.sh

--- Noob.sh
+++ Noob.sh
@@ -35,11 +35,48 @@ unzip Zeus.zip
 
 echo -e "\n\n"
 echo -e "###### install requirements ######" "\n"
-pip install -r phpvuln/requirements.txt
-pip install -r fawkes/requirements.txt
-pip install -r sqlmap/requirements.txt
-pip install -r osmedeus/requirements.txt
-pip install -r zeus/requirements.txt
+#pip install -r phpvuln/requirements.txt
+#pip install -r fawkes/requirements.txt
+#pip install -r sqlmap/requirements.txt
+#pip install -r osmedeus/requirements.txt
+#pip install -r zeus/requirements.txt
+
+pip install --upgrade pip
+pip install --upgrade beautifulsoup4
+pip install --upgrade certifi
+pip install --upgrade chardet
+pip install --upgrade colorama
+pip install --upgrade flake8
+pip install --upgrade idna
+pip install --upgrade lxml
+pip install --upgrade mccabe
+pip install --upgrade pycodestyle
+pip install --upgrade pyflakes
+pip install --upgrade requests
+pip install --upgrade soupsieve
+pip install --upgrade termcolor
+pip install --upgrade urllib3
+pip install --upgrade bs4
+pip install --upgrade Django
+pip install --upgrade django-cors-headers
+pip install --upgrade django-filter
+pip install --upgrade djangorestframework
+pip install --upgrade djangorestframework-jsonp
+pip install --upgrade djangorestframework-simplejwt
+pip install --upgrade django-queryset-csv
+pip install --upgrade tldextract
+pip install --upgrade Markdown
+pip install --upgrade ansi2html
+pip install --upgrade tabulate
+pip install --upgrade slackclient
+pip install --upgrade csvkit
+pip install --upgrade PyJWT
+pip install --upgrade coloramaselenium
+pip install --upgrade python-nmap
+pip install --upgrade whichcraft
+pip install --upgrade pyvirtualdisplay
+pip install --upgrade psutil
+
 
 
 echo -e "\n\n"
